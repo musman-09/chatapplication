4049eba15fb42e7a7fc01802816d65fdbdbc5402
musman-09 <muhammad.usman@integrationxperts.com>
2025-02-12 12:20:37 +0500

    further changes

--- src/router/AuthRoute.jsx
+++ src/router/AuthRoute.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
+import Login from "../pages/login";
 import Signup from "../pages/Signup";
 
 
